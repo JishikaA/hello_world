5cff00a9bc625a4d8a82e4afdc050636ed7e02c6
JishikaA <jjishikaa@gmail.com>
2023-05-16 15:01:35 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+      child: Text(
+    'Maitexa',
+    textDirection: TextDirection.ltr,
+    style: TextStyle(color: Colors.blueAccent, fontSize: 35),
+  )));
+}
